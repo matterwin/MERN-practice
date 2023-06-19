5041c41fe085ad55d370e44002d5109d08df31cf
Matthew Erwin <matthewderwin23@gmail.com>
2023-06-18 22:01:42 -0500

    landing page done

--- starter/client/src/pages/Landing.js
+++ starter/client/src/pages/Landing.js
@@ -1,9 +1,10 @@
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage';
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
         <nav>
             <img src={logo} alt='jobify' className='logo'/>
         </nav>
@@ -15,11 +16,11 @@ const Landing = () => {
                 <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
-                <button>Login/Register</button>
+                <button className='btn btn-hero'>Login/Register</button>
             </div>
             <img src={main} alt='job hunt' className='img main-img'/>
         </div>
-    </main>
+    </Wrapper>
   )
 }
 
